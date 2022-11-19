a9f31f5ae09f617415ee3d766fe92553e20146da
oburgosm <oscarbma@ext.inditex.com>
2022-11-19 20:18:45 +0100

    Create project structure

---
+++ src/main/java/com/capgemini/edem/DemoDgsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.edem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDgsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capgemini/edem/DemoDgsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.edem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDgsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
