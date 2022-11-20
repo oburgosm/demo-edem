9fa6329fa0264c8e14d65b550bc73c306241ba9a
Óscar Burgos Martín <oscar.burgos-martin@capgemini.com>
2022-11-20 21:19:26 +0100

    Add Mongo Repository (#7)
    * Add mongo starter
    
    * Implemets PRoduct fetcher

--- src/main/java/com/capgemini/edem/dgs/datafetchers/ProductDatafetcher.java
+++ src/main/java/com/capgemini/edem/dgs/datafetchers/ProductDatafetcher.java
@@ -1,17 +1,29 @@
 package com.capgemini.edem.dgs.datafetchers;
 
+import com.capgemini.edem.dgs.DgsConstants.QUERY;
 import com.capgemini.edem.dgs.types.Product;
+import com.capgemini.edem.mongo.ProductRepository;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
-import graphql.schema.DataFetchingEnvironment;
+import com.netflix.graphql.dgs.InputArgument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
 
 @DgsComponent
 public class ProductDatafetcher {
+  
+  private static final Logger LOG = LoggerFactory.getLogger(ProductDatafetcher.class);
+  
+  @Autowired
+  private ProductRepository productRepository;
+  
   @DgsData(
-      parentType = "Query",
-      field = "product"
+      parentType = QUERY.TYPE_NAME,
+      field = QUERY.Product
   )
-  public Product getProduct(DataFetchingEnvironment dataFetchingEnvironment) {
-    return null;
+  public Mono<Product> getProduct(@InputArgument String id) {
+    return this.productRepository.findById(id);
   }
 }
--- src/main/java/com/capgemini/edem/dgs/datafetchers/ProductsDatafetcher.java
+++ src/main/java/com/capgemini/edem/dgs/datafetchers/ProductsDatafetcher.java
@@ -1,18 +1,25 @@
 package com.capgemini.edem.dgs.datafetchers;
 
+import com.capgemini.edem.dgs.DgsConstants.QUERY;
 import com.capgemini.edem.dgs.types.Product;
+import com.capgemini.edem.mongo.ProductRepository;
 import com.netflix.graphql.dgs.DgsComponent;
 import com.netflix.graphql.dgs.DgsData;
 import graphql.schema.DataFetchingEnvironment;
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
 
 @DgsComponent
 public class ProductsDatafetcher {
+  
+  @Autowired
+  private ProductRepository productRepository;
+  
   @DgsData(
-      parentType = "Query",
-      field = "products"
+      parentType = QUERY.TYPE_NAME,
+      field = QUERY.Products
   )
-  public List<Product> getProducts(DataFetchingEnvironment dataFetchingEnvironment) {
-    return null;
+  public Flux<Product> getProducts(DataFetchingEnvironment dataFetchingEnvironment) {
+    return this.productRepository.findAll();
   }
 }
